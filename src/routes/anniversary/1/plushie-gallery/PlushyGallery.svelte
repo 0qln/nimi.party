29684a9fc1f82332eea8239e294f9700c675fa4a
oq_ <129159411+0qln@users.noreply.github.com>
2026-01-17 17:33:14 +0100

    Plushy pics (#11)
    * implement plushy gallery
    
    * masonry layout
    
    * place holder for loading plushy pics\
    
    * adjust loading prio

---
+++ src/routes/anniversary/1/plushie-gallery/PlushyGallery.svelte
@@ -0,0 +1,23 @@
+<svelte:options customElement="plushy-gallery" />
+
+<script lang="ts">
+  import type { PlushyPhotoDatum } from "./types";
+
+  interface Props {
+    data: Array<PlushyPhotoDatum>;
+    cols?: number;
+  }
+
+  let { data, cols = 3 }: Props = $props();
+</script>
+
+<div
+  class="w-full space-y-1"
+  style="column-count: {cols}; column-gap: var(--spacing);"
+>
+  {#each data as photo}
+    <div class="break-inside-avoid w-full">
+      <photo.component {...photo.props} />
+    </div>
+  {/each}
+</div>
